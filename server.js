2335fd73a9627a5e3fef59dc28b27e04c6f77598
Inna Widener <iwide2019@gmail.com>
2020-05-20 00:31:41 -0500

    Added lines to require mongoose and dotenv. Set up connection to mongodb. Removed Component on lne 1 in App.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +15,17 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+// app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/doctors",
+ {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
+.then(() => console.log("DB Connected!"));
+
 
 // Send every other request to the React app
 // Define any API routes before this runs
