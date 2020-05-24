c246b9a91887e35c7c91844ad158f766721db81b
Inna Widener <iwide2019@gmail.com>
2020-05-24 18:56:23 -0500

    Style added to Navbar. Footer, Grid and Jumbotron with codes added to components folder.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "../src/components/Navbar";
+import Footer from "./components/Footer";
 import "./index.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
           <Route exact path="/home" component={Home} />
       </div>
+      <Footer />
     </Router>
   );
 }
---
+++ client/src/components/Grid/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function Container({ fluid, children }) {
+  return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+
+export function Row({ fluid, children }) {
+  return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+
+// This Col component lets us size bootstrap columns with less syntax
+// e.g. <Col size="md-12"> instead of <div className="col-md-12">
+export function Col({ size, children }) {
+  return (
+    <div
+      className={size
+        .split(" ")
+        .map(size => "col-" + size)
+        .join(" ")}
+    >
+      {children}
+    </div>
+  );
+}
