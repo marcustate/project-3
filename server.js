e4668dec19ed63b45013731136145be2d95f4424
marcustate <58437889+marcustate@users.noreply.github.com>
2020-05-26 16:32:50 -0500

    added login.js and routes.js

--- server.js
+++ server.js
@@ -2,23 +2,34 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+​
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-// Define API routes here
-
-// Send every other request to the React app
+​
+// Add routes
+// app.use(routes);
+​
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/doctors",
+ {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+})
+.then(() => console.log("DB Connected!"));
+​
+​
+// Send every request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
-app.listen(PORT, () => {
+​
+app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
