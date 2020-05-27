ec8a08511f9cdfe993c5a1e506498cf6f2ed8ddc
marcustate <58437889+marcustate@users.noreply.github.com>
2020-05-26 21:29:43 -0500

    added header and react bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Tracker from "../src/pages/Tracker";
 import Search from "../src/pages/Search";
 import GeneralInformation from "../src/pages/GeneralInformation";
 import SearchButtons from "../src/pages/SearchButtons";
+import Footer from "../src/components/Footer";
 
 function App() {
   return (
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
+
 
 function Header() {
     return (
